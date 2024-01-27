004a5397589dba4ceabad6d4c16eded24df4a7de
Kauabunga <Kauabunga@users.noreply.github.com>
2024-01-27 02:06:54 +0000

    deploy: 41f8a7dbd0a7aa0e5f0b84c9ecb82639308f4b85

--- _next/static/QEBwGV-MaGwVS49nqMKC5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/213-0d652d01b2189e60.js","static/chunks/pages/index-61f48aae3885f2e7.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
